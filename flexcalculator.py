0c928054bd4d52d5b434facd0922a1b8f43016e5
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-05-15 16:08:11 +0400

    Done

--- flexcalculator.py
+++ flexcalculator.py
@@ -1,1 +1,10 @@
-name = "Osama"
+list_array = []
+
+inputedNumber = input("Enter the Number: ")
+
+while inputedNumber != "done":
+    inputedNumber = input("Enter the Number: ")
+    list_array.append(inputedNumber)
+    
+print(list_array[:-1])
+
